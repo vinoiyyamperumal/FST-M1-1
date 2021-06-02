f4a7ede0d913d4966155c1d31ac766dc0fc3cbbd
vinoiyyamperumal <84365954+vinoiyyamperumal@users.noreply.github.com>
2021-06-02 10:44:05 +0530

    Add files via upload

---
+++ TestNG/Activities/TestNGSuiteExample.java
@@ -0,0 +1,19 @@
+package examples;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNGSuiteExample {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
